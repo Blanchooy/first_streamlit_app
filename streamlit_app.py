ab7223a4bd2bcde237417856a10caea285694fd2
Blanchooy <144209431+Blanchooy@users.noreply.github.com>
2023-12-06 15:55:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title ('My Moms New Healthy Diner')
 
-streamlit.header ('🥣 Breakfast Favotites')
-streamlit.text ('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text ('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text ('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Favotites')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
